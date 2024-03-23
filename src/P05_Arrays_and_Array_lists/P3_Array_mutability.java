c2c993e7b6171e7d72aaa7defc7f12a0d237d776
Suman Saurabh Das <dsumansaurabh@gmail.com>
2024-03-23 12:43:40 +0530

    Arrays - 03 Array mutability

---
+++ src/P05_Arrays_and_Array_lists/P3_Array_mutability.java
@@ -0,0 +1,20 @@
+package P05_Arrays_and_Array_lists;
+
+import java.util.Arrays;
+
+public class P3_Array_mutability {
+    public static void main(String[] args) {
+        int[] arr = {10, 19, 38, 27, 60, 45};
+        System.out.println(Arrays.toString(arr));
+        modifyArr(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void modifyArr(int[] nums) {
+        nums[0] = 100;
+    }
+}
+/*
+    In java arrays are mutable i.e. their values can be modified.
+    Here nums and arr point to the same object in heap, hence when we change the value of nums[0] the object gets modified and since arr points to the same object, we see the change for arr as well.
+*/
